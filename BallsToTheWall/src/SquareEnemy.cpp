7c72a85a15354757ba2d2d04596bccbfef61e7f1
Muncherkin <48158637+Muncherkin@users.noreply.github.com>
2021-03-24 14:32:18 +0100

    Square Update

--- BallsToTheWall/src/SquareEnemy.cpp
+++ BallsToTheWall/src/SquareEnemy.cpp
@@ -6,10 +6,12 @@ SquareEnemy::SquareEnemy(const sf::Vector2f& aPosition, const float& aRotation) 
 	if (myAxis) 
 	{
 		myExtent = (Random::Float() - 0.5f) * Window::GetSize().x * 0.9f;
+		myTime = asin(aPosition.y / (Window::GetSize().y * 0.4f));
 	}
 	else 
 	{
 		myExtent = (Random::Float() - 0.5f) * Window::GetSize().y * 0.9f;
+		myTime = asin(aPosition.x / (Window::GetSize().x * 0.45f));
 	}
 	myTime = Random::Float() * Math::Pi;
 	mySpeed = (1 + Random::Float() * 0.5f);
@@ -22,10 +24,10 @@ void SquareEnemy::OnStart()
 
 void SquareEnemy::OnUpdate()
 {
-	myTime += TimeTracker::GetDeltaTime() * mySpeed;
+	myTime += TimeTracker::GetDeltaTime() * mySpeed * (!myStarting);
 	sf::Vector2f tempGoal = sf::Vector2f(
-		(myAxis) * myExtent + (!myAxis && !myStarting) * sin(myTime) * Window::GetSize().x * 0.45f + (myStarting && !myAxis) * myShape.getPosition().x,
-		(!myAxis) * myExtent + (myAxis && !myStarting) * sin(myTime) * Window::GetSize().y * 0.4f + (myStarting && myAxis) * myShape.getPosition().y);
+		(myAxis) * myExtent + (!myAxis) * sin(myTime) * Window::GetSize().x * 0.45f,
+		(!myAxis) * myExtent + (myAxis) * sin(myTime) * Window::GetSize().y * 0.4f);
 	myShape.setPosition(Math::Lerp(myShape.getPosition(), tempGoal, 0.3f / Math::Distance(myShape.getPosition(), tempGoal) * TimeTracker::GetTimeScale()));
 	if (Math::Distance(myShape.getPosition(), tempGoal) < 1) 
 	{
